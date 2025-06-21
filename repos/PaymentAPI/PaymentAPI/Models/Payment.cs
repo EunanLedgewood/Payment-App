4b79ff37431edddb29995561bea393e8a9bcc70a
Eunan Ledgewood <eledgewood2001@gmail.com>
2025-06-21 18:38:53 +0100

    Updating Payment Model

--- repos/PaymentAPI/PaymentAPI/Models/Payment.cs
+++ repos/PaymentAPI/PaymentAPI/Models/Payment.cs
@@ -1,13 +1,13 @@
-﻿namespace PaymentAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
 {
-    using System.ComponentModel.DataAnnotations;
     public class Payment
     {
-        
-        public int Id { get; set; } //This is the Primary Key
+        public int Id { get; set; } // Primary Key
 
         [Required]
-        [Range(0.01,double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
         public decimal Amount { get; set; }
 
         [Required]
@@ -19,10 +19,10 @@
         public string Receiver { get; set; }
 
         [Required]
-        public DateTime DateOfPayment { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow; // Changed from DateOfPayment to Date
 
         [Required]
         [StringLength(50)]
-        public string PaymentMethod { get; set; }
+        public string PaymentMethod { get; set; } = "Transfer";
     }
 }
